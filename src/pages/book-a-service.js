e6959a501e05ae35d9e32675757d32ea15a9a1ef
Hamdi Lachaal <hklachaal@gmail.com>
2020-05-12 17:54:01 -0700

    fix review paragraph position

--- src/pages/book-a-service.js
+++ src/pages/book-a-service.js
@@ -3,7 +3,6 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import Welcome from "../components/widget/welcome"
 import Appointment from "./../components/book-a-service/appointment"
 import Reviews from "./../components/widget/reviews"
 import Sponsors from "../components/widget/sponsors"
@@ -12,7 +11,6 @@ const BookingPage = () => {
   return (
     <Layout>
       <SEO title="Book a service" />
-      <Welcome />
       <Appointment />
       <Reviews />
       <Sponsors />
